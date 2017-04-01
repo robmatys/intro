10864987fea23a02e7d0b8f94c87361fb94344ad
robmatys <robmatys@gmail.com>
2017-03-31 23:10:35 -0400

    .js testing

---
+++ html/types.js
@@ -0,0 +1,13 @@
+var myString = "Bob";
+var myNumber = 15;
+var myBoolean = false;
+
+//Do not edit the code under this line
+console.log("Name " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good Joke? " + myBoolean);
+
+console.log("Talk to me?");
+console.log(35+3.5);
+
+console.log(Math.random());
